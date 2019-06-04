62eabf41c07863e9943ba807dbefea4fcbfcf043
studio7ph <philstudio7@gmail.com>
2019-06-04 10:38:58 +0300

    Add test connection & to do in main

--- src/main/java/TourCRM.java
+++ src/main/java/TourCRM.java
@@ -2,6 +2,8 @@ import control.Controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+
 public class TourCRM extends Application {
 
 
@@ -13,5 +15,15 @@ launch(args);
        public void start(Stage primaryStage) throws Exception {
               Controller controller = new Controller();
               controller.start(primaryStage);
+              ConnectionDriver connectionDriver = new ConnectionDriver(); //test connection
+              //to do
+              //Database architecture - Phil
+              //data input - Vlad
+              //tours collections - Vlad
+              //clients collections - Vlad
+              //saving collections to database - Phil
+              //database queries - Phil
+              //displaying results of database queries - Phil
+
        }
 }
